c315d55abaece42e593a816b9f2b52a2b582c62c
Ed Vinyard <ed.vinyard@gmail.com>
2018-03-13 10:19:14 -0500

    attempt to ensure SQL Server Windows Service is running prior to running tests

--- RunAllTests.ps1
+++ RunAllTests.ps1
@@ -1,2 +1,12 @@
+$sqlService = Get-Service MSSQLSERVER
+
+while ($sqlService.Status -ne 'Running')
+{
+	Write-Warning "SQL Server isn't running.  Trying to start it..."
+    Start-Service MSSQLSERVER
+    Start-Sleep -seconds 5
+    $sqlService.Refresh()
+}
+
 $testAssemblies = gci -Recurse -Include "*Test.dll" | where { $_.FullName.Contains("\bin\") }
 nunit3-console.exe --out=TestStdout.log --err=TestStderr.log $testAssemblies
